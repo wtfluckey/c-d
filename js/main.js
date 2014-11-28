1b190a051d16c1057816b97b106f21eac419a234
Aly Fluckey <fluckeya@gmail.com>
2014-11-28 14:32:12 -0500

    3steps to apply box is now collapsable under 700px

--- js/main.js
+++ js/main.js
@@ -9,9 +9,15 @@ $(function() {
 
   $(window).resize(function(){
       var w = $(window).width();
-      if(w > 320 && menu.is(':hidden')) {
+      if(w > 960 && menu.is(':hidden')) {
           menu.removeAttr('style');
       }
+  });
+
+  $('.icon-3steps').click(function(e) {
+    e.preventDefault();
+    $('.apply-steps-box-info').slideToggle();
+    $('.icon-3steps').toggleClass('fa-sort-asc fa-sort-desc', 'fa-sort-asc');
   });
 });
 
